c4b5e1456d35163d72851c522ac157f39a0820dc
aasim21 <aasimsaeed20@gmail.com>
2025-03-12 16:46:00 +0530

    Updated the Login File

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,36 +1,6 @@
-//Components
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Nav from "../../components/Navbar/Nav";
-//Hooks
-import { useFirebase } from "../../context/Fiebase";
-import { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
 
 //Functional Component
 const LoginPage = () => {
-
-  //Calling hooks
-  const fireBase = useFirebase();
-  const emailRef = useRef();
-  const passRef = useRef();
-  const navigate = useNavigate();
-
-  //function to handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fireBase.LoginUser(
-      emailRef.current.value,
-      passRef.current.value
-    );
-};
-useEffect(()=>{
-    if(fireBase.isLoggedIn){
-        //navigate to home
-        navigate("/");
-    }
-},[fireBase.isLoggedIn]);
-
   return (
     <>
     </>
